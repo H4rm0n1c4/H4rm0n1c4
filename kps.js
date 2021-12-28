ca1f8e9afacc87180c70d82dc7d389fb9611f1df
H4rm0n1c4 <96792221+H4rm0n1c4@users.noreply.github.com>
2021-12-28 20:33:37 +0200

    Add files via upload

---
+++ kps.js
@@ -0,0 +1,64 @@
+/*
+function kivi (){
+    let txt;
+    let pelaaja;
+    let tietokone;
+    if (confirm("Choose Rock?"))  {
+        txt = "SMASHING VICTORY!!";
+        pelaaja = document.getElementById("pelaaja").innerHTML +"I";
+        tietokone = document.getElementById("tietokone").innerHTML;
+    
+    } 
+    else {
+        txt = "Choose another!";
+        pelaaja = document.getElementById("pelaaja").innerHTML;
+        tietokone = document.getElementById("tietokone").innerHTML;
+    }
+    document.getElementById("tietokone").innerHTML = tietokone;
+    document.getElementById("pelaaja").innerHTML = pelaaja;
+document.getElementById("Prompt").innerHTML = txt;
+
+}
+function sakset (){
+    let txt;
+    let pelaaja;
+    let tietokone;
+    if (confirm("Choose Scissors?"))  {
+        txt = "Couldnt make the cut this time!";
+        pelaaja= document.getElementById("pelaaja").innerHTML;
+        tietokone= document.getElementById("tietokone").innerHTML +"I";
+        
+    } else {
+        txt = "Choose another!";
+        pelaaja= document.getElementById("pelaaja").innerHTML;+" "
+    tietokone= document.getElementById("tietokone").innerHTML +" ";}
+    document.getElementById("tietokone").innerHTML = tietokone;
+    document.getElementById("pelaaja").innerHTML = pelaaja;
+document.getElementById("Prompt").innerHTML = txt;
+    }
+
+function paperi (){
+    let txt;
+    let pelaaja;
+    let tietokone;
+    if (confirm("Choose Paper?"))  {
+        txt = "YOU FOLD!";
+        pelaaja= document.getElementById("pelaaja").innerHTML;
+        tietokone= document.getElementById("tietokone").innerHTML +"I";
+        
+    } else {
+        txt = "Choose another!";
+        pelaaja= document.getElementById("pelaaja").innerHTML;+" "
+    tietokone= document.getElementById("tietokone").innerHTML +" ";}
+    document.getElementById("tietokone").innerHTML = tietokone;
+    document.getElementById("pelaaja").innerHTML = pelaaja;
+document.getElementById("Prompt").innerHTML = txt;
+    }
+    */
+   function weapons() {
+    let pelaaja1;
+    let pelaaja2;
+if  (prompt("Player 1, Choose your Weapon! (Rock/Paper/Scissors)"));
+(prompt("Player 2, choose your Weapon!(Rock/Paper/Scissors"));
+
+   }
