255d83485a94d572cc4bba50de260dddf7ee6ba3
H4rm0n1c4 <96792221+H4rm0n1c4@users.noreply.github.com>
2021-12-29 16:08:14 +0200

    Add files via upload
    Working ver 1.0

--- kps.js
+++ kps.js
@@ -1,64 +1,66 @@
-/* Testing simple button interactions
-function kivi (){
-    let txt;
-    let pelaaja;
-    let tietokone;
-    if (confirm("Choose Rock?"))  {
+function choose(playerChoice){
+
+let txt;
+let pelaaja;
+let tietokone;
+
+const options = ['Rock', 'Paper', 'Scissors']
+
+if (confirm("Choose " + playerChoice + "?"))  {
+    const aiRandomIndex = getRandomInt(3)
+const aiChoice = options[aiRandomIndex]
+
+document.getElementById('ai-choice').innerHTML = aiChoice
+
+const result = checkWinner(playerChoice, aiChoice)
+
+    if (result === 'Win'){
         txt = "SMASHING VICTORY!!";
         pelaaja = document.getElementById("pelaaja").innerHTML +"I";
         tietokone = document.getElementById("tietokone").innerHTML;
-    
-    } 
-    else {
-        txt = "Choose another!";
-        pelaaja = document.getElementById("pelaaja").innerHTML;
+    }
+    if (result === 'Loss'){
+        txt = "Better luck next time chump!";
+        pelaaja = document.getElementById("pelaaja").innerHTML ;
+        tietokone = document.getElementById("tietokone").innerHTML + "I";
+    }
+    if (result === 'Draw'){
+        txt = "No winners here!";
+        pelaaja = document.getElementById("pelaaja").innerHTML ;
         tietokone = document.getElementById("tietokone").innerHTML;
     }
-    document.getElementById("tietokone").innerHTML = tietokone;
-    document.getElementById("pelaaja").innerHTML = pelaaja;
+    
+} 
+else {
+    txt = "Choose another!";
+    pelaaja = document.getElementById("pelaaja").innerHTML;
+    tietokone = document.getElementById("tietokone").innerHTML;
+}
 document.getElementById("Prompt").innerHTML = txt;
+document.getElementById("tietokone").innerHTML = tietokone;
+document.getElementById("pelaaja").innerHTML = pelaaja;
 
 }
-function sakset (){
-    let txt;
-    let pelaaja;
-    let tietokone;
-    if (confirm("Choose Scissors?"))  {
-        txt = "Couldnt make the cut this time!";
-        pelaaja= document.getElementById("pelaaja").innerHTML;
-        tietokone= document.getElementById("tietokone").innerHTML +"I";
-        
-    } else {
-        txt = "Choose another!";
-        pelaaja= document.getElementById("pelaaja").innerHTML;+" "
-    tietokone= document.getElementById("tietokone").innerHTML +" ";}
-    document.getElementById("tietokone").innerHTML = tietokone;
-    document.getElementById("pelaaja").innerHTML = pelaaja;
-document.getElementById("Prompt").innerHTML = txt;
-    }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  function checkWinner(playerChoice, aiChoice){
+console.log(playerChoice + '' + aiChoice)
 
-function paperi (){
-    let txt;
-    let pelaaja;
-    let tietokone;
-    if (confirm("Choose Paper?"))  {
-        txt = "YOU FOLD!";
-        pelaaja= document.getElementById("pelaaja").innerHTML;
-        tietokone= document.getElementById("tietokone").innerHTML +"I";
-        
-    } else {
-        txt = "Choose another!";
-        pelaaja= document.getElementById("pelaaja").innerHTML;+" "
-    tietokone= document.getElementById("tietokone").innerHTML +" ";}
-    document.getElementById("tietokone").innerHTML = tietokone;
-    document.getElementById("pelaaja").innerHTML = pelaaja;
-document.getElementById("Prompt").innerHTML = txt;
+if (playerChoice === 'Rock'){
+if (aiChoice === 'Paper') return 'Loss';
+if (aiChoice === 'Rock') return 'Draw';
+if (aiChoice === 'Scissors') return 'Win';
+}
+if (playerChoice === 'Paper'){
+    if (aiChoice === 'Paper') return 'Draw';
+    if (aiChoice === 'Rock') return 'Win';
+    if (aiChoice === 'Scissors') return 'Loss';
     }
-    */
-   function weapons() {
-    let pelaaja1;
-    let pelaaja2;
-if  (prompt("Player 1, Choose your Weapon! (Rock/Paper/Scissors)"));
-(prompt("Player 2, choose your Weapon!(Rock/Paper/Scissors"));
-
-   }
+    if (playerChoice === 'Scissors'){
+        if (aiChoice === 'Paper') return 'Win';
+        if (aiChoice === 'Rock') return 'Loss';
+        if (aiChoice === 'Scissors') return 'Draw';
+        }
+   
+  }
